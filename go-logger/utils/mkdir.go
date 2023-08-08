924569072a982063d9ea78430396d8e51220f9a2
pokeya <pokeya.mystic@gmail.com>
2023-08-08 17:08:44 +0800

    docs(go-logger): Update license headers in multiple files

--- go-logger/utils/mkdir.go
+++ go-logger/utils/mkdir.go
@@ -1,3 +1,8 @@
+// Copyright 2023 Pokeya. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+// Package logger provides a simple, lightweight logging library for Go.
 package utils
 
 import (
