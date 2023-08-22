e3947ddc31035327057cc6ade770848b816728c4
pokeya <pokeya.mystic@gmail.com>
2023-08-22 10:48:29 +0800

    docs(suprelog): Generate GoDoc

---
+++ suprelog/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2023 Pokeya. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+//go:generate go doc
+package suprelog
